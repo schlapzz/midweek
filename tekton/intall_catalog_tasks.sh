4a5195227dc7bc605f97dadad0bc3090f4533238
Christian Schlatter <schlatter@puzzle.ch>
2020-12-01 11:55:25 +0100

    init new

--- tekton/intall_catalog_tasks.sh
+++ tekton/intall_catalog_tasks.sh
@@ -3,9 +3,19 @@
 echo "create git-clone task"
 oc create -f https://raw.githubusercontent.com/tektoncd/catalog/master/task/git-clone/0.2/git-clone.yaml
 
-echo "create buildah build task"
-oc create -f tasks/buildah-build.yaml
+echo "create buildah & trivy task"
+oc create -f build-scan-push-task.yaml
 
-echo "create buildah push task"
-oc create -f tasks/buildah-push.yaml
+echo "create test pipeline"
+oc create -f pipeline.yaml
 
+echo "create docker registry secret"
+oc create secret docker-registry \
+    --docker-server=docker.io \
+    --docker-username=schlapzz \
+    --docker-password=${DOCKER_PASSWORD} \
+    --docker-email=schlatter@puzzle.ch \
+    regcred
+
+echo "link regcred"
+oc secrets link pipeline regcred
